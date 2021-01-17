c8da92ea727f186d9c1ce642b617a1665768a420
hfyeomans <hank.yeomans@me.com>
2021-01-17 03:04:51 +0000

    updated import modules to include alternate way

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import random
 import hangman_words
-import hangman_art
+from hangman_art import logo, stages
 #TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
 #Delete this line: word_list = ["ardvark", "baboon", "camel"]
 word_list = hangman_words.word_list
@@ -13,7 +13,7 @@ end_of_game = False
 lives = 6
 
 #TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-print(hangman_art.logo)
+print(logo)
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
@@ -26,7 +26,7 @@ while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
     #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-    if guess in chosen_word:
+    if guess in display:
         print(f"You have already selected letter {guess}")
     #Check guessed letter
     for position in range(word_length):
@@ -53,4 +53,4 @@ while not end_of_game:
         print("You win.")
 
     #TODO-2: - Import the stages from hangman_art.py and make this error go away.
-    print(hangman_art.stages[lives])
+    print(stages[lives])
